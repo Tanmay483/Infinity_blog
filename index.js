10f415f6bc0ca1739a4355ebbee6f3832b636071
Tanmay Makwana <makwanatanmay483@gmail.com>
2023-06-15 15:55:13 +0530

    nothing

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
+
 
 app.use(bodyParser.json());
 
