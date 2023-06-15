d565f14b968ca7d9fe002a7cb14f66a091c80b33
Tanmay Makwana <makwanatanmay483@gmail.com>
2023-06-15 15:54:44 +0530

    genral change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Infinity Blog' });
