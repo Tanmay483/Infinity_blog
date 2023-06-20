13d01760b31336870245befd7489ff510194d159
Tanmay Makwana <makwanatanmay483@gmail.com>
2023-06-20 15:22:11 +0530

    update blog

--- app/routes/blogs.routes.js
+++ app/routes/blogs.routes.js
@@ -17,56 +17,62 @@ module.exports = (app) => {
 
     // Update a blog with id
     router.put('/:bId', multer, (req, res) => {
-        let bId = req.params.bId;
-        const cId = req.body.cId;
-        const iParentCatID = req.body.iParentCatID;
-        const vBlogTitle = req.body.vBlogTitle;
-        const vBlogTitleSlug = req.body.vBlogTitleSlug;
-        const vBlogDescription = req.body.vBlogDescription;
-        const tCreatedDate = req.body.tCreatedDate;
-        const tUpdatedDate = req.body.tUpdatedDate;
-      
-        // Check if images are provided
-        let vBlogFeatureImage = '';
-        let vBlogThumbnailImage = '';
-      
-        if (req.files && req.files.length > 0) {
-          vBlogFeatureImage = "http://localhost:8080/" + req.files[0].path.replace(/\\/g, '/');
-          vBlogThumbnailImage = "http://localhost:8080/" + req.files[1].path.replace(/\\/g, '/');
+      let bId = req.params.bId;
+      const cId = req.body.cId;
+      const iParentCatID = req.body.iParentCatID;
+      const vBlogTitle = req.body.vBlogTitle;
+      const vBlogTitleSlug = req.body.vBlogTitleSlug;
+      const vBlogDescription = req.body.vBlogDescription;
+      const tCreatedDate = req.body.tCreatedDate;
+      const tUpdatedDate = req.body.tUpdatedDate;
+    
+      // Check if images are provided
+      let vBlogFeatureImage = '';
+      let vBlogThumbnailImage = '';
+    
+      if (req.files && req.files.length > 0) {
+        vBlogFeatureImage = "http://localhost:8080/" + req.files[0].path.replace(/\\/g, '/');
+        vBlogThumbnailImage = "http://localhost:8080/" + req.files[1].path.replace(/\\/g, '/');
+      } else {
+        if (req.body.url) {
+          vBlogFeatureImage = req.body.url;
+          vBlogThumbnailImage = req.body.url;
         }
-      
-        
-        var sql =
-          "UPDATE `tbl_blogs` SET `cId`='" +
-          cId +
-          "',`iParentCatID`='" +
-          iParentCatID +
-          "',`vBlogTitle`= '" +
-          vBlogTitle +
-          "',`vBlogTitleSlug`='" +
-          vBlogTitleSlug +
-          "',`vBlogDescription`='" +
-          vBlogDescription +
-          "',`tCreatedDate`='" +
-          tCreatedDate +
-          "',`tUpdatedDate` = '" +
-          tUpdatedDate +
-          "',`vBlogFeatureImage`='" +
-          vBlogFeatureImage +
-          "',`vBlogThumbnailImage`='" +
-          vBlogThumbnailImage +
-          "' WHERE  bId = '" +
-          bId +
-          "' ";
-      
-        conn.query(sql, (err, data) => {
-          if (err) throw err;
-          console.log('blog changed successfully');
-          console.log(req.body);
-        });
-      
-        res.send('blog changed successfully');
+      }
+    
+      var sql =
+        "UPDATE `tbl_blogs` SET `cId`='" +
+        cId +
+        "',`iParentCatID`='" +
+        iParentCatID +
+        "',`vBlogTitle`= '" +
+        vBlogTitle +
+        "',`vBlogTitleSlug`='" +
+        vBlogTitleSlug +
+        "',`vBlogDescription`='" +
+        vBlogDescription +
+        "',`tCreatedDate`='" +
+        tCreatedDate +
+        "',`tUpdatedDate` = '" +
+        tUpdatedDate +
+        "',`vBlogFeatureImage`='" +
+        vBlogFeatureImage +
+        "',`vBlogThumbnailImage`='" +
+        vBlogThumbnailImage +
+        "' WHERE  bId = '" +
+        bId +
+        "' ";
+    
+      conn.query(sql, (err, data) => {
+        if (err) throw err;
+        console.log('blog changed successfully');
+        console.log(req.body);
       });
+    
+      res.send('blog changed successfully');
+    });
+    
+    
       
     // Delete a blog with id
     router.delete('/:bId', blog.delete);
