5f0f61ebcf02d14174f74c8bbd36071be2e9b50c
Tanmay Makwana <makwanatanmay483@gmail.com>
2023-09-23 18:30:23 +0530

    new api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+const helmet = require('helmet')
+
 
 app.use(cors());
 
@@ -14,8 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Infinity Blog' });
 });
+// app.use(helmet())
 app.use('/app/Images', express.static(path.join(__dirname, 'app/Images')));
-
+app.use(require("prerender-node").set("prerenderToken", ["2dbxFxTmhDo8d2Vlv5wW"]));
 
 require('./app/routes/user.routes')(app);
 require('./app/routes/catageories.routes')(app);
@@ -23,7 +26,7 @@ require('./app/routes/blogs.routes')(app);
 require('./app/routes/system.routes')(app);
 require('./app/routes/description.routs')(app);
 require('./app/routes/contanct.routes')(app);
-require('./app/routes/modes.routes')(app)
+require('./app/routes/modes.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
